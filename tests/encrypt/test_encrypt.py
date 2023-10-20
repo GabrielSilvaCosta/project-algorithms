5d7f33cf96ea7184223a9afc10168b64255be78a
GabrielSilvaCosta <bob@>
2023-10-20 19:30:03 -0300

    atualizando requisito 2 e 3

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("flamengo", 2) == "ognema_lf"
+
+    assert encrypt_message("flamengo", 3) == "alf_ognem"
+
+    assert encrypt_message("flamengo", 10) == "ognemalf"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("flamengo", "a")
