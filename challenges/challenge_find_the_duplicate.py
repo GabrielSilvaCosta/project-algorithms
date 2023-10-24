51febf4e2d0e1c7fef9a00ecbe3d60233ced4f17
GabrielSilvaCosta <bob@>
2023-10-24 09:14:41 -0300

    corrigindo bugs requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,6 +2,9 @@ def find_duplicate(nums):
     if not nums or not isinstance(nums, list) or len(nums) < 2:
         return False
 
+    if any(not isinstance(num, (int, float)) or num < 0 for num in nums):
+        return False
+
     nums.sort()
 
     for i in range(len(nums) - 1):
